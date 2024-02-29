415c78e68d54d3db6a47cfc34ba5ef7b3d86fa7a
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-02-29 09:33:03 -0400

    Siendo UT, se aplicara el impuesto correspondiente

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,5 +1,13 @@
-function calculadora(cantidad, precio){
-    return cantidad * precio;
-}
+function calculadora(cantidad, precio, estado){
+    let precioneto=cantidad * precio;
+    if(estado=="UT")
+    {
+        return precioneto*0.065 + precioneto;
+    }
+    return precioneto;
+};
+
+
+    
 
 export default calculadora;
--- src/calculadora.spec.js
+++ src/calculadora.spec.js
@@ -4,4 +4,7 @@ describe("Determinar el precio total", () => {
     it("Si ingreso la cantidad y el precio del item, me retorna el precio neto", () => {
         expect(calculadora(2, 100)).toEqual(200);
     });
+    it("Si ingreso UT, me debe devolver el precio con el impuesto", () => {
+        expect(calculadora(2, 100, "UT")).toEqual(213);
+    });
 });
