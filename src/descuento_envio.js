3cce4675e1129cb6bee3db03f60f610fbc2edd70
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-03-06 20:19:55 -0800

    Se agrega la funcionalidad para aplicar un descuento por envio de 1% si el cliente es de tipo Antiguo Recurrente

--- src/descuento_envio.js
+++ src/descuento_envio.js
@@ -3,6 +3,8 @@ function descontarDeEnvio(cliente){
         return 0;
     }else if(cliente=="Recurrente"){
         return 0.005;
+    }else if(cliente=="Antiguo"){
+        return 0.01;
     }
 }
 export default descontarDeEnvio;
