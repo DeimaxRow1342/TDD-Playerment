2d2f14c65772bb7c64d027cf00df3d53aff0f917
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-03-06 22:38:56 -0400

    Se agrego a la interfas la funcionalidad de precio de envio total, siendo la multiplicacion de la cantidad por el precio de envio unitario

--- src/presenter.js
+++ src/presenter.js
@@ -1,31 +1,34 @@
 import calculadora from "./calculadora.js";
 import descontar from "./descuento.js";
 import impuesto from "./impuesto.js";
+import calcular_precio_envio from "./precio_envio.js";
 
 const cantidad = document.querySelector("#cantidad");
 const precio = document.querySelector("#precio");
 const estado = document.getElementById("estado");
 const producto = document.getElementById("producto");
-const Pesovolumetrico = document.getElementById("#PesoVolumetrico");
+const peso = document.querySelector("#PesoVolumetrico");
 const form = document.querySelector("#calcular-form");
 const precioNeto = document.querySelector("#neto-div");
 const descuentoAplicado = document.querySelector("#descuento-div");
 const impuestoAplicado = document.querySelector("#impuesto-div");
-const Preciodiv = document.querySelector("#precioenvio-div");
+const precio_envio = document.querySelector("#precioenvio-div");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const pesoV = Number.parseInt(peso.value);
   const cantidadNumero = Number.parseInt(cantidad.value);
   const precioNumero = Number.parseInt(precio.value);
   const estadoTexto = estado.value;
   const productoCategoria = producto.value;
   
+ 
 
   precioNeto.innerHTML = "<p> Precio neto: (" + cantidadNumero + "*$" + precioNumero + "): $" + cantidadNumero*precioNumero + "</p>";
   descuentoAplicado.innerHTML = "<p> Descuento (" + descontar(cantidadNumero*precioNumero, productoCategoria)*100 + "%): $" + descontar(cantidadNumero*precioNumero, productoCategoria) * (cantidadNumero*precioNumero) + "</p>";
   impuestoAplicado.innerHTML = "<p> Impuesto para " + estadoTexto + " (%" + impuesto(estadoTexto, productoCategoria)*100 + "): $" + impuesto(estadoTexto, productoCategoria)*(cantidadNumero*precioNumero) + "</p>";
-  Preciodiv.innerHTML = "<p> Precio de envio total: $ 0" + "</p>";
-  div.innerHTML = "<p> Precio total(descuento e impuesto): $" + calculadora(cantidadNumero, precioNumero, estadoTexto, productoCategoria) + "</p>";
+  precio_envio.innerHTML = "<p> Precio envio total:" + calcular_precio_envio(pesoV, cantidadNumero) + "</p>";
+  div.innerHTML = "<p> Precio total(descuento e impuesto): $" + calculadora(cantidadNumero, precioNumero, estadoTexto, productoCategoria, pesoV) + "</p>";
 });
